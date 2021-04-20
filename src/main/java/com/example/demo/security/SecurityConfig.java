e89a84c00dd43776fac288ad27dc04c7119063de
perdi8 <perdi666@gmail.com>
2021-04-20 11:33:14 +0200

    bug CORS

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-        http.csrf().disable().authorizeRequests().antMatchers("/**/auth").permitAll().antMatchers().permitAll()
+        http.csrf().disable().authorizeRequests().antMatchers("/**/auth").permitAll()
 //                .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().cors();
