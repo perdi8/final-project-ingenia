45a8703724857f9b88c8426789a0b622d52d1625
perdi8 <perdi666@gmail.com>
2021-04-26 10:31:38 +0200

    update-changes

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
         http.csrf().disable().authorizeRequests().antMatchers("/**/auth").permitAll()
-//                .anyRequest().authenticated()
+                .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().cors();
 
