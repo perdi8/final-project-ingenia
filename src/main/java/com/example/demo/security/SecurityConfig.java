81694325eb6e8495229d0135586f0e2ffd345ce6
perdi8 <perdi666@gmail.com>
2021-05-10 17:47:56 +0200

    permision register

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -32,8 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
         http.csrf().disable().authorizeRequests().antMatchers("/auth/**").permitAll().and()
-                .authorizeRequests().antMatchers("/api/**")
-                .authenticated()
+                .authorizeRequests().antMatchers("/api/**").permitAll()
+//                .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().cors();
 
